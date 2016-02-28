b9531f7d527bd3681c1ea4bff4571400e1f2abde
Anthony Green <akoaysigod@gmail.com>
2016-02-27 19:37:14 -0500

    retrying protocol nodes

--- src/public/GENode.swift
+++ src/public/GENode.swift
@@ -11,6 +11,125 @@ import GLKit
 import Metal
 import QuartzCore
 
+typealias Nodes = [Node]
+
+protocol Node: class {
+  var name: String? { get set }
+
+  var size: CGSize { get set }
+  var width: Float { get }
+  var height: Float { get }
+
+  var x: Float { get set }
+  var y: Float { get set }
+  var position: (x: Float, y: Float) { get set }
+  var zPosition: Int { get set }
+  var z: Float { get }
+
+  var anchorPoint: (x: Float, y: Float) { get set }
+
+  var rotation: Float { get set }
+
+  var scale: (x: Float, y: Float) { get set }
+  var xScale: Float { get set }
+  var yScale: Float { get set }
+
+  var modelMatrix: GLKMatrix4 { get }
+
+  typealias T
+  var parent: T? { get set }
+  var nodes: [T] { get set }
+
+  var time: CFTimeInterval { get set }
+  func updateWithDelta(delta: CFTimeInterval)
+
+  var action: GEAction? { get set }
+  var hasAction: Bool { get }
+  func runAction(action: GEAction)
+}
+
+extension Node {
+  var width: Float {
+    return 1.0
+  }
+
+  var height: Float {
+    return 1.0
+  }
+
+  var position: (x: Float, y: Float) {
+    get { return (x, y) }
+    set {
+      x = newValue.0
+      y = newValue.1
+    } 
+  }
+
+  var z: Float {
+    return -1.0 * Float(zPosition / Int.max)
+  }
+
+  var scale: (x: Float, y: Float) {
+    get { return (xScale, yScale) }
+    set {
+      xScale = newValue.x
+      yScale = newValue.y
+    }
+  }
+
+  var modelMatrix: GLKMatrix4 {
+    let x = self.x - (width * anchorPoint.x)
+    let y = self.y - (height * anchorPoint.y)
+
+    let xRot = 0.0 - (width * anchorPoint.x)
+    let yRot = 0.0 - (height * anchorPoint.y)
+
+    let scale = GLKMatrix4MakeScale(xScale, yScale, 1.0)
+    let worldTranslate = GLKMatrix4MakeTranslation(x - xRot, y - yRot, z)
+    let rotation = GLKMatrix4MakeRotation(GLKMathDegreesToRadians(-1 * self.rotation), 0.0, 0.0, 1.0)
+    let rotationTranslate = GLKMatrix4MakeTranslation(xRot, yRot, z)
+
+    return worldTranslate * rotation * rotationTranslate * scale
+  }
+
+  func updateWithDeltaTime(delta: CFTimeInterval) {
+    time += delta
+
+    guard let action = self.action else { return }
+    if !action.completed {
+      //action.run(self, delta: delta)
+    }
+  }
+
+  var hasAction: Bool {
+    return false
+    //let parentHasAction = getSuperParent()?.hasAction ?? false
+    //return self.action != nil || parentHasAction
+  }
+
+  func runAction(action: GEAction) {
+    self.action = action
+  }
+
+//    //tree stuff
+//  func addNode<T: Node>(node: T) {
+//    //node.parent = self
+//    //self.nodes.append(node)
+//  }
+//  
+//  func getSuperParent<T: Node>() -> T? {
+//    var parent = self.parent
+//    while true {
+//      if let root = parent?.parent {
+//        parent = root
+//      }
+//      else {
+//        return parent
+//      }
+//    }
+//  }
+}
+
 typealias GENodes = [GENode]
 
 public class GENode {
@@ -70,8 +189,6 @@ public class GENode {
     return worldTranslate * rotation * rotationTranslate * scale
   }
 
-  let uniqueID = NSUUID().UUIDString
-  
   var parent: GENode?
   var nodes = GENodes()
   
--- src/public/GERenderNode.swift
+++ src/public/GERenderNode.swift
@@ -16,6 +16,25 @@ protocol Renderable {
   func draw(commandBuffer: MTLCommandBuffer, renderEncoder: MTLRenderCommandEncoder, sampler: MTLSamplerState?)
 }
 
+extension Renderable {
+//  func decompose(matrix: GLKMatrix4) -> GLKMatrix4 {
+//    let parentRotScale = GLKMatrix4GetMatrix3(matrix)
+//    let selfRotScale = GLKMatrix4GetMatrix3(self.modelMatrix)
+//    let rotScale = parentRotScale * selfRotScale
+//    
+//    let parentTranslate = GLKMatrix4GetColumn(matrix, 3)
+//    let selfTranslate = GLKMatrix4GetColumn(self.modelMatrix, 3)
+//    let translate = parentTranslate + selfTranslate
+//   
+//    let firstColumn = GLKVector4MakeWithVector3(GLKMatrix3GetColumn(rotScale, 0), translate.x)
+//    let secondColumn = GLKVector4MakeWithVector3(GLKMatrix3GetColumn(rotScale, 1), translate.y)
+//    let thirdColumn = GLKVector4MakeWithVector3(GLKMatrix3GetColumn(rotScale, 2), self.z)
+//    let fourthColumn = GLKVector4(v: (0.0, 0.0, 0.0, 1.0))
+//    
+//    return GLKMatrix4MakeWithRows(firstColumn, secondColumn, thirdColumn, fourthColumn)
+//  }
+}
+
 typealias GERenderNodes = [GERenderNode]
 
 public class GERenderNode: GENode, Renderable {
@@ -50,24 +69,24 @@ public class GERenderNode: GENode, Renderable {
 
     self.uniformBufferQueue = BufferQueue(device: self.device, dataSize: FloatSize * self.modelMatrix.data.count)
   }
-  
+
   func decompose(matrix: GLKMatrix4) -> GLKMatrix4 {
     let parentRotScale = GLKMatrix4GetMatrix3(matrix)
     let selfRotScale = GLKMatrix4GetMatrix3(self.modelMatrix)
     let rotScale = parentRotScale * selfRotScale
-    
+  
     let parentTranslate = GLKMatrix4GetColumn(matrix, 3)
     let selfTranslate = GLKMatrix4GetColumn(self.modelMatrix, 3)
     let translate = parentTranslate + selfTranslate
-   
+  
     let firstColumn = GLKVector4MakeWithVector3(GLKMatrix3GetColumn(rotScale, 0), translate.x)
     let secondColumn = GLKVector4MakeWithVector3(GLKMatrix3GetColumn(rotScale, 1), translate.y)
     let thirdColumn = GLKVector4MakeWithVector3(GLKMatrix3GetColumn(rotScale, 2), self.z)
     let fourthColumn = GLKVector4(v: (0.0, 0.0, 0.0, 1.0))
-    
+  
     return GLKMatrix4MakeWithRows(firstColumn, secondColumn, thirdColumn, fourthColumn)
   }
-  
+   
   func draw(commandBuffer: MTLCommandBuffer, renderEncoder: MTLRenderCommandEncoder, sampler: MTLSamplerState? = nil) {
     renderEncoder.setVertexBuffer(self.vertexBuffer, offset: 0, atIndex: 0)
     
