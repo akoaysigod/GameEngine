dc9f0e4542b495a4bb9e1a96c40eea4e03d30a97
Anthony Green <akoaysigod@gmail.com>
2016-12-24 13:45:20 -0800

    create macos target

---
+++ GameEngine-macOS/GameEngine-macOS.h
@@ -0,0 +1,19 @@
+//
+//  GameEngine-macOS.h
+//  GameEngine-macOS
+//
+//  Created by Anthony Green on 12/24/16.
+//  Copyright © 2016 Anthony Green. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GameEngine-macOS.
+FOUNDATION_EXPORT double GameEngine_macOSVersionNumber;
+
+//! Project version string for GameEngine-macOS.
+FOUNDATION_EXPORT const unsigned char GameEngine_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameEngine_macOS/PublicHeader.h>
+
+
