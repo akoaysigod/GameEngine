8143e5cdf6797404451320fed2518f8755257736
Anthony Green <akoaysigod@gmail.com>
2016-12-28 18:41:20 -0500

    clean up updater code

--- Sources/private/Updater.swift
+++ Sources/private/Updater.swift
@@ -9,53 +9,59 @@
 import Foundation
 import QuartzCore
 
+#if !os(macOS)
+  typealias DisplayLink = CADisplayLink
+#else
+  typealias DisplayLink = CVDisplayLink
+#endif
+
 final class Updater {
   private weak var gameView: GameView?
-  private var previousTime = 0.0
-
-  #if os(iOS)
-  private var displayLink: CADisplayLink!
-  #else
-  private var displayLink: CVDisplayLink?
-  #endif
+  fileprivate var previousTime = 0.0
+  fileprivate var displayLink: DisplayLink?
 
   init(gameView: GameView) {
     self.gameView = gameView
-
-    #if !os(macOS)
-      displayLink = CADisplayLink(target: self, selector: #selector(newFrame(_:)))
-      displayLink.add(to: .main, forMode: .commonModes)
-    #else
-      func callback(link: CVDisplayLink,
-                    inNow: UnsafePointer<CVTimeStamp>, //wtf is this?
-                    inOutputTime: UnsafePointer<CVTimeStamp>,
-                    flagsIn: CVOptionFlags,
-                    flagsOut: UnsafeMutablePointer<CVOptionFlags>,
-                    displayLinkContext: UnsafeMutableRawPointer?) -> CVReturn {
-        unsafeBitCast(displayLinkContext, to: Updater.self).update(time: CACurrentMediaTime())
-        return kCVReturnSuccess
-      }
-
-      CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
-      guard let displayLink = displayLink else {
-        fatalError("Unable to create a CVDisplayLink?")
-      }
-      CVDisplayLinkSetOutputCallback(displayLink, callback, unsafeBitCast(self, to: UnsafeMutableRawPointer.self))
-      CVDisplayLinkStart(displayLink)
-    #endif
+    createDisplayLink()
   }
 
-  func update(time: Double) {
+  fileprivate func update(time: Double) {
     if previousTime == 0.0 {
       previousTime = time
     }
     gameView?.update(delta: time - previousTime)
     previousTime = time
   }
-
-  #if !os(macOS)
+}
+extension Updater {
+#if !os(macOS)
+  func createDisplayLink() {
+    displayLink = CADisplayLink(target: self, selector: #selector(newFrame(_:)))
+    displayLink?.add(to: .main, forMode: .commonModes)
+  }
+  
   @objc private func newFrame(_ displayLink: CADisplayLink) {
     update(time: displayLink.timestamp)
   }
-  #endif
+#else
+  func createDisplayLink() {
+    func callback(link: CVDisplayLink,
+                  inNow: UnsafePointer<CVTimeStamp>, //wtf is this?
+                  inOutputTime: UnsafePointer<CVTimeStamp>,
+                  flagsIn: CVOptionFlags,
+                  flagsOut: UnsafeMutablePointer<CVOptionFlags>,
+                  displayLinkContext: UnsafeMutableRawPointer?) -> CVReturn {
+      unsafeBitCast(displayLinkContext, to: Updater.self).update(time: CACurrentMediaTime())
+      return kCVReturnSuccess
+    }
+  
+    CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+    guard let displayLink = displayLink else {
+      fatalError("Unable to create a CVDisplayLink?")
+    }
+    CVDisplayLinkSetOutputCallback(displayLink, callback, unsafeBitCast(self, to: UnsafeMutableRawPointer.self))
+    CVDisplayLinkStart(displayLink)
+  }
+#endif
 }
+
